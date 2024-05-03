4e2ed54c53e6887f22515267f859932e37bee89d
Luis Felipe Rossi <lfcr93@gmail.com>
2024-05-02 23:22:28 -0300

    iniciando repo

---
+++ src/main/java/com/example/t1Projaq/T1ProjaqApplication.java
@@ -0,0 +1,13 @@
+package com.example.t1Projaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1ProjaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1ProjaqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/t1Projaq/T1ProjaqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t1Projaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1ProjaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
